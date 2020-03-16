Fixing dEdW derivative

activation.py
@@ -44,7 +44,7 @@ class Activation(Layer):
         ########## (E3) Your code goes here ##########
         # print("self is of type: {}".format(type(self)))
         # print("Input delta has shape:{}".format(delta_n.shape))
-        delta = np.multiply(delta_n, sigmoid_prime(self.input_data))
+        delta = np.multiply(delta_n, self.act_prime(self.input_data))
         #delta  =  derivative of the loss * sigmoid prime(layer)
 
         #delta^l = {delta^{l+1}(W^{l+1}}\odot sigmoid_prime(U^l)  # equation 22

layer.py
@@ -53,6 +53,12 @@ class FCLayer(Layer):
         self.weights = Parameter(weights)               # instantiate Parameter object by passing the initialized values
         self.bias = Parameter(bias)                     # instantiate Parameter object by passing the initialized values
         self.param = [self.weights, self.bias]
+        self.forward_outputs = []
+        self.backprop_deltas = []
+        self.all_weights = []
+        self.all_weights_grads = []
+        self.all_bias_grads = []
+        self.backward_counter = 0
 
         # print("Weights are: {}".format(self.weights.value))
         # print("Shape of weights matrix is: {}".format(self.weights.value.shape))
@@ -68,7 +74,7 @@ class FCLayer(Layer):
 
         # store the weight and bias to a list
 
-    def forward(self, input_data, mode):
+    def forward(self, input_data, mode=True):
         # print("Inside forward propagation of FCLayer ")
         """
         If self.weights.shape = (in, out), then
@@ -96,6 +102,7 @@ class FCLayer(Layer):
         # print("Weights are:{}".format(self.weights.value))
         # print("Output(U):{}".format(output))
         self.output = output
+        self.forward_outputs.append(output)
         # print("Shape of the forward output (U): {}".format(output.shape))
         ##########            end           ##########
 
@@ -114,10 +121,12 @@ class FCLayer(Layer):
         # print("Input delta_n shape: {}".format(delta_n.shape))
         ########## (E2) Your code goes here ##########
         delta = np.dot(delta_n, self.weights.value.T)
+
         # print("Shape of self.output is: {}".format(self.output.shape))
-        dEdW = np.dot(self.output.T, delta).T #H^{l-1}*\delta^l
+        dEdW = np.dot(self.input_data.T, delta_n) #H^{l-1}*\delta^l
         # print("Shape of 1s vector:{}".format(np.ones((list(delta_n.shape)[0],1)).shape))
-        dEdb = np.dot(np.ones((list(delta_n.shape)[0],1)).T, delta_n)
+        dEdb = np.dot(np.ones((delta_n.shape[0], 1)).T, delta_n)
+        # dEdb = np.dot(np.ones((list(delta_n.shape)[0],1)).T, delta_n)
         #dEdb^L = 1^T\delta^L where delta^L is of dimensions n x m^L
         ##########            end           ##########
 
@@ -126,6 +135,11 @@ class FCLayer(Layer):
         # print("Calculated dEdb shape:{}".format(dEdb.shape))
 
         # Store gradients
+        self.backprop_deltas.append(delta)
+        self.all_weights.append(self.weights.value)
+        self.all_weights_grads.append(dEdW)
+        self.all_bias_grads.append(dEdb)
+
         self.weights.grad = dEdW
         self.bias.grad = dEdb
         return delta

optim.py
@@ -60,8 +60,14 @@ class SGD(Optimizer):
 
         # which norm to use in gradient clipping (default is 2-norm)
         self.ord = self.kwargs['ord'] if 'ord' in self.kwargs else 2
+        self.nesterov_descents = []
+        self.param_values = []
+        self.descent_updates = []
+        self.empty_param_grads = []
+        self.sgd_flow = []
 
     def step(self):
+        self.sgd_flow.append("inside step function")
         # clip gradient values (see torch.nn.utils.clip_grad_norm_ function)
         total_norm = 0
         if self.clipvalue is not None:
@@ -78,29 +84,28 @@ class SGD(Optimizer):
         for param in self.param_lst:
             # no updates happen when there's no gradient information
             if param.grad is None:
+                self.empty_param_grads.append("Empty param.grad")
                 continue
 
             # momentum or Nesterov Accelerated Gradient update
             if self.momentum or self.nesterov:
                 v_prev = param.velocity.copy()
+                self.sgd_flow.append(" if self.momentum or self.nesterov, v_prev = {} ".format(v_prev))
                 if self.momentum:
                     param.velocity = self.mu * v_prev - self.learning_rate * param.grad
                     descent = param.velocity
+                    self.sgd_flow.append("self.momentum is true, param.velocity = {}\
+                                        descent = {}".format(param.velocity, descent))
+
+
 
                 # Implement Nesterov accelarted gradient
                 ########## (E5) Your code goes here ##########
                 else:
-                    # print("param.grad is: {}".format(param.grad))
-                    # print("v_prev is: {}".format(v_prev))
+                    print("Nesterov Accelerated SGD")
                     param.velocity = self.mu * v_prev - self.learning_rate * param.grad
-                    descent = param.velocity
-
-
-                    #reference: https://github.com/hero9968/PaddlePaddle-book/blob/1ff47b284c565d030b198705d5f18b4bd4ce53e5/python/paddle/v2/fluid/tests/test_momentum_op.py
-                    # param - grad * learning_rate - \
-                    # velocity_out * mu * learning_rate
-                    #velocity[layer] = gamma * velocity[layer] + alpha * grad[layer]
-                    # model[layer] += velocity[layer]
+                    descent = -self.mu * v_prev + (1 + self.mu) * param.velocity
+                    self.nesterov_descents.append(descent)
                 ##########            end           ##########
 
             # ordinary gradient descent (w/o momentum)
@@ -108,6 +113,8 @@ class SGD(Optimizer):
                 descent = -self.learning_rate * param.grad
 
             # update
+            self.param_values.append(param.value)
+            self.descent_updates.append(descent)
             param.value += descent
 
 class Adam(Optimizer):
@@ -116,7 +123,6 @@ class Adam(Optimizer):
                  beta2=0.999,
                  eps=1e-8,
                  clipvalue=None):
-        # print("Initializing an Adam optimizer with learning rate: {} ...".format(lr))
         """
         An Adam optimizer
         :param parameters:          (list) list of Paramters of the neural network
@@ -128,14 +134,10 @@ class Adam(Optimizer):
         # Link the Optimizer with Parameters, and set up some attributes
         super(Adam, self).__init__(parameters, lr, clipvalue=clipvalue)
 
-
         # Initialize Parameter.m_t and Parameter.v_t: moving average of first and second moments of gradient
         for param in self.param_lst:
-            # print("There are {} elements in param_lst".format(len(self.param_lst)))
             param.m_t = np.zeros(param.shape, dtype=np.float32)
-            # print("param.m_t: {}".format(param.m_t))
             param.v_t = np.zeros(param.shape, dtype=np.float32)
-            # print("param.v_t: {}".format(param.v_t))
             param.step = 0
         self.beta1 = beta1
         self.beta2 = beta2
@@ -148,10 +150,6 @@ class Adam(Optimizer):
             bias_correction1 = 1 - self.beta1 ** param.step
             bias_correction2 = 1 - self.beta2 ** param.step
 
-            # print("type of self.beta1 shape is:{}".format(type(self.beta1)))
-            # print("self.beta1 = {}".format(self.beta1))
-            # print("param.m_t shape is:{}".format(param.m_t.shape))
-            # print("param.grad shape is:{}".format(param.grad.shape))
             param.m_t = self.beta1 * param.m_t + (1 - self.beta1) * param.grad
             param.v_t = self.beta2 * param.v_t + (1 - self.beta2) * (param.grad**2)
 
@@ -188,4 +186,3 @@ class RMSProp(Optimizer):
             param.meansquare = self.decay_rate * param.meansquare + (1 - self.decay_rate) * param.grad**2
             descent = -self.learning_rate * param.grad / (np.sqrt(param.meansquare) + 1e-6)
             param.value += descent
-